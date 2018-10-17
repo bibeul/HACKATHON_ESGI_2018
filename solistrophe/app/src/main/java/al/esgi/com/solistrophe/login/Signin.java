1924532771510d6a086a65117eeb73a9ce44dc6e
Unknown <dormoy.guillaume@gmail.com>
2018-10-18 00:21:22 +0200

    updated underline in edit text

--- solistrophe/app/src/main/java/al/esgi/com/solistrophe/login/Signin.java
+++ solistrophe/app/src/main/java/al/esgi/com/solistrophe/login/Signin.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import al.esgi.com.solistrophe.R;
 
@@ -18,10 +19,14 @@ public class Signin extends AppCompatActivity {
         Button signIn = findViewById(R.id.signInButton);
         Button register = findViewById(R.id.registerButton);
 
+        final EditText login = findViewById(R.id.loginText);
+        final EditText password = findViewById(R.id.passwordText);
+
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                System.out.println(login.getText());
+                System.out.println(password.getText());
             }
         });
 
