e3f45ac4b19dd59f5174b9253c64ecaf33e7927c
dormoy <dormoy.guillaume@gmail.com>
2018-10-17 18:19:25 +0200

    first commit

---
+++ solistropheApi/app/src/main/java/al/esgi/com/solistrophe/SignIn.java
@@ -0,0 +1,13 @@
+package al.esgi.com.solistrophe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
