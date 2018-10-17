516efd7eecd0693b73542b839eca220c98d6e2d9
Unknown <dormoy.guillaume@gmail.com>
2018-10-17 23:34:40 +0200

    create register page

---
+++ solistrophe/app/src/main/java/al/esgi/com/solistrophe/login/Register.java
@@ -0,0 +1,16 @@
+package al.esgi.com.solistrophe.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import al.esgi.com.solistrophe.R;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+}
---
+++ solistrophe/app/src/main/java/al/esgi/com/solistrophe/login/Signin.java
@@ -0,0 +1,38 @@
+package al.esgi.com.solistrophe.login;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import al.esgi.com.solistrophe.R;
+
+public class Signin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+        Button signIn = findViewById(R.id.signInButton);
+        Button register = findViewById(R.id.registerButton);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Signin.this, Register.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
