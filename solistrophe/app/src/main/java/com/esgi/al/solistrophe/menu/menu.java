e4c37aaa315918c8a660cd4a3592b17118f7c511
Guillaume Tako <guillaumetako@yahoo.fr>
2018-10-18 09:53:10 +0200

    Ajout menu activity

---
+++ solistrophe/app/src/main/java/com/esgi/al/solistrophe/menu/menu.java
@@ -0,0 +1,15 @@
+package com.esgi.al.solistrophe.menu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.esgi.al.solistrophe.R;
+
+public class menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
