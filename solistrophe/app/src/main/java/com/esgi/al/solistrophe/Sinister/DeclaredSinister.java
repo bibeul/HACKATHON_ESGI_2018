bbfda2a132f9a79be6be713295839a493c646df2
dormoy <dormoy.guillaume@gmail.com>
2018-10-18 21:53:25 +0200

    start creating sinister

---
+++ solistrophe/app/src/main/java/com/esgi/al/solistrophe/Sinister/DeclaredSinister.java
@@ -0,0 +1,16 @@
+package com.esgi.al.solistrophe.Sinister;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.esgi.al.solistrophe.R;
+
+public class DeclaredSinister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_declared_sinister);
+
+    }
+}
